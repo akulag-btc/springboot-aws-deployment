addc6d8dd6c6b71e7bdc2b4975234f2c534191ea
akulag-btc <akulag@boston-technology.com>
2026-06-30 12:48:26 +0530

    first commit

---
+++ src/main/java/springboot_aws_deployment/springboot_aws_deployment/SpringbootAwsDeploymentApplication.java
@@ -0,0 +1,13 @@
+package springboot_aws_deployment.springboot_aws_deployment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootAwsDeploymentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootAwsDeploymentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/springboot_aws_deployment/springboot_aws_deployment/SpringbootAwsDeploymentApplicationTests.java
@@ -0,0 +1,13 @@
+package springboot_aws_deployment.springboot_aws_deployment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootAwsDeploymentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
